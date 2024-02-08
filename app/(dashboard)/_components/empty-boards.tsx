dc4d6fc29c37e0c116b52d74f125981a608d8ac9
Puru-Rana <pururana24@gmail.com>
2024-02-08 00:46:13 -0500

    12: Querying

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -5,8 +5,10 @@ import { api } from "@/convex/_generated/api";
 import { useOrganization } from "@clerk/nextjs";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const EmptyBoards = () => {
+  const router = useRouter();
   const { mutate, pending } = useApiMutation(api.board.create);
   const { organization } = useOrganization();
 
@@ -19,7 +21,7 @@ export const EmptyBoards = () => {
     })
       .then((id) => {
         toast.success("Board created ");
-        // TODO : Redirect to board / {id}
+        router.push(`/board/${id}`);
       })
       .catch(() => toast.error("Failed to create board"));
   };
