145d9b8f593f99b2b428501d1149470b3d1cd59a
Puru-Rana <pururana24@gmail.com>
2024-02-10 02:05:00 -0500

    15: Room Auth & Info

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,41 @@
+import { api } from "@/convex/_generated/api";
+import { auth, currentUser } from "@clerk/nextjs";
+import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient } from "convex/browser";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+const liveblocks = new Liveblocks({
+  secret:
+    "sk_dev_G66IQ0tmOt02qTq2Lphj1AvcIstFIVaiZKgWt7WhZ3mCSPPTFdJddOqm2t_apTav",
+});
+
+export async function POST(request: Request) {
+  const authorization = await auth();
+  const user = await currentUser();
+
+  if (!authorization || !user) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const { room } = await request.json();
+  const board = await convex.query(api.board.get, { id: room });
+
+  if (board?.orgId !== authorization.orgId) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const userInfo = {
+    name: user.firstName || "Teammate",
+    picture: user.imageUrl,
+  };
+
+  const session = liveblocks.prepareSession(user.id, { userInfo: userInfo });
+
+  if (room) {
+    session.allow(room, session.FULL_ACCESS);
+  }
+
+  const { status, body } = await session.authorize();
+  return new Response(body, { status });
+}
--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -5,13 +5,13 @@ import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
 
 interface CanvasProps {
-  boardId: String;
+  boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <Toolbar />
     </main>
