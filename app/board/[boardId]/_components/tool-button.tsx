94b514dbdfa562973a4db4807d983ae270bb23f9
Puru-Rana <pururana24@gmail.com>
2024-02-10 13:15:19 -0500

    17: Room Toolbar

---
+++ app/board/[boardId]/_components/tool-button.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { LucideIcon } from "lucide-react";
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+
+interface ToolButtonProps {
+  label: string;
+  icon: LucideIcon;
+  onClick: () => void;
+  isActive?: boolean;
+  isDisbaled?: boolean;
+}
+export const ToolButton = ({
+  label,
+  icon: Icon,
+  onClick,
+  isActive,
+  isDisbaled,
+}: ToolButtonProps) => {
+  return (
+    <Hint label={label} side="right" sideOffset={14}>
+      <Button
+        disabled={isDisbaled}
+        onClick={onClick}
+        size="icon"
+        variant={isActive ? "boardActive" : "board"}
+      >
+        <Icon />
+      </Button>
+    </Hint>
+  );
+};
