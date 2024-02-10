81f3666d443076b1a52e643697e833b77ee0b38d
Puru-Rana <pururana24@gmail.com>
2024-02-09 22:59:58 -0500

    14: Liveblocks

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,7 +1,13 @@
 export const Info = () => {
   return (
     <main className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-        TODO : Information about the board 
+      TODO : Information about the board
     </main>
+  );
+};
+
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px] " />
   );
 };
