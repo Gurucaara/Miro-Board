d771463fe971143fb448e10dd868cc4e8f3a245e
Puru-Rana <pururana24@gmail.com>
2024-02-10 18:09:56 -0500

    19: Cursors Presence

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { Camera } from "@/types/canvas";
+
 const COLORS = [
   "#DC2626",
   "#649E7D",
@@ -17,9 +19,19 @@ const COLORS = [
 ];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 export function connectionIdToColor(connectionId: number): string {
   return COLORS[connectionId % COLORS.length];
-}
+};
+
+export function pointerEventToCanvasPoint(
+  e: React.PointerEvent,
+  camera: Camera,
+) {
+  return {
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+};
