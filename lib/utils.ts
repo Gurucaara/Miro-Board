25b86c782357c08a182b6730a6fa3c1b4511b215
Puru-Rana <pururana24@gmail.com>
2024-02-10 12:02:59 -0500

    16: Room Participants

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = [
+  "#DC2626",
+  "#649E7D",
+  "#FFD166",
+  "#06D6A0",
+  "#118AB2",
+  "#073B4C",
+  "#E63946",
+  "#1D3557",
+  "#DC2626",
+  "#D97706",
+  "#059669",
+  "#7C3AED",
+];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
 }
