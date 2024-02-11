27e59e4d32e700a88bce0db94ad3f13eba54ce85
Puru-Rana <pururana24@gmail.com>
2024-02-11 14:21:30 -0500

    22: Select Box

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -27,6 +27,7 @@ import { Toolbar } from "./toolbar";
 import { Participants } from "./participants";
 import { CursorsPresence } from "./cursors-presence";
 import { LayerPreview } from "./layer-preview";
+import { SelectionBox } from "./selection-box";
 
 const MAX_LAYERS = 100;
 
@@ -195,6 +196,7 @@ export const Canvas = ({ boardId }: CanvasProps) => {
               selectionColor={layerIdsToColorSelection[layerId]}
             />
           ))}
+          <SelectionBox onResizeHandlePointerDown={() => {}} />
           <CursorsPresence />
         </g>
       </svg>
